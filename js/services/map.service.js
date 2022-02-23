54622f06b7fc010809af8e11d6f77d030007eaf7
AmitMi1 <amit.am130@gmail.com>
2022-02-24 01:38:25 +0200

    .

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -20,11 +20,11 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
         .then(() => {
             gMap = new google.maps.Map(
                 document.querySelector('#map'), {
-                    center: { lat, lng },
-                    zoom: 15
-                })
+                center: { lat, lng },
+                zoom: 15
+            })
             addMapListener()
-                // addIconListener()
+            // addIconListener()
             locService.getLocs().then(locs => {
                 if (!locs.length) return
                 addMarkers(locs)
@@ -34,7 +34,7 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
 
 function addIconListener() {
     var elIcons = document.querySelectorAll('.icon')
-    elIcons.forEach(icon => (addEventListener("click", function() {
+    elIcons.forEach(icon => (addEventListener("click", function () {
         console.log(icon.attributes.src)
     })))
 }
@@ -73,10 +73,9 @@ function panTo(lat, lng) {
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
     const API_KEY = 'AIzaSyB3stN2xyUcqTxDrMmu4T_WHL7sZSIQN-s'
-        //Private API key
+    //Private API key
     var elGoogleApi = document.createElement('script')
-    elGoogleApi.src = `
-                    https: //maps.googleapis.com/maps/api/js?key=${API_KEY}`
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true;
     document.body.append(elGoogleApi)
 
@@ -95,7 +94,7 @@ function addMapListener() {
     gMap.addListener("click", (mapsMouseEvent) => {
         var locationName = prompt('Enter location name')
         if (!locationName.trim()) return
-            // gId = saveLocation(mapsMouseEvent.latLng, locationName)
+        // gId = saveLocation(mapsMouseEvent.latLng, locationName)
         locService.getLocByLatLng(
             mapsMouseEvent.latLng.lat(),
             mapsMouseEvent.latLng.lng(),
@@ -111,5 +110,5 @@ function addMapListener() {
         // appController.saveLoc(mapsMouseEvent.latLng, locationName)
     })
     infoWindow = new google.maps.InfoWindow()
-        // return Promise.resolve()
+    // return Promise.resolve()
 }
