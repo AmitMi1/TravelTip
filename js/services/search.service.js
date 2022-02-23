2d6364e7d47b2eab614efab54140077de8bbb825
Eloose <elooseghantous@gmail.com>
2022-02-23 16:21:13 +0200

    added searchservic

--- js/services/search.service.js
+++ js/services/search.service.js
@@ -1,9 +1,18 @@
-export const searchService {
+import storageService from 'storage.service.js'
 
+export const searchService = {
+    getPosByName: getLocationByName,
 }
 const GEOCODE_API = 'AIzaSyC5hMbe-8DIFINoWf3CRnTZNAmbd07NxVs'
 
-function search(input) {
-    return axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyC5hMbe-8DIFINoWf3CRnTZNAmbd07NxVs')
-        .then(())
+function getLocationByName(input) {
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${input}&key=${GEOCODE_API}`)
+        .then(res => res.data.location)
 }
+
+
+
+
+
+
+// https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
