c5d3bff09e805c3cdc0e91e3c4d833525d40662a
AmitMi1 <amit.am130@gmail.com>
2022-02-25 03:27:45 +0200

    .

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -10,7 +10,7 @@ const KEY = '5a14a6a860e9d68fa91c90933c51e1e3'
 
 function getWeatherForLocation(location) {
     return axios
-        .get(`http://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lng}&units=metric&APPID=${KEY}`)
+        .get(`https://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lng}&units=metric&APPID=${KEY}`)
         .then((res) => {
             return res.data
         })
